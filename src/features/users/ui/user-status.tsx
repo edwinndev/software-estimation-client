62dfac87fd27785fb35d729c6c7df10ed7c37b8a
Juan Carlos Vargas <jcvargas.dev@gmail.com>
2026-09-16 10:46:15 -0500

    feat: add icon to user status component

--- src/features/users/ui/user-status.tsx
+++ src/features/users/ui/user-status.tsx
@@ -1,9 +1,18 @@
+import { CircleUserRound } from "lucide-react"
 import { Badge } from "@/components/ui/badge"
 
 export const UserStatus = ({ isActive }: { isActive: boolean }) => {
   if (isActive) {
-    return <Badge variant="default">Cuenta Activa</Badge>
+    return (
+      <Badge variant="default">
+        <CircleUserRound className="mr-2" /> Cuenta Activa
+      </Badge>
+    )
   }
 
-  return <Badge variant="destructive">Cuenta Suspendida</Badge>
+  return (
+    <Badge variant="destructive">
+      <CircleUserRound className="mr-2" /> Cuenta Suspendida
+    </Badge>
+  )
 }
