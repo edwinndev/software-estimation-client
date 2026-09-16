78685f76795e620f3294882b9c679b3bd014a6e7
Juan Carlos Vargas <jcvargas.dev@gmail.com>
2026-09-16 10:46:29 -0500

    ref: remove unused user status component

--- src/features/users/ui/user-status.tsx
+++
@@ -1,7 +0,0 @@
-export const UserStatus = ({ isActive }: { isActive: boolean }) => {
-  return (
-    <span className={isActive ? "text-green-500" : "text-red-500"}>
-      {isActive ? "Activo" : "Suspendido"}
-    </span>
-  )
-}
